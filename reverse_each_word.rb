110f6d8e0f05b670447e2d48862db0f42cbb49ef
Hannah Kofkin <hkofkin@gmail.com>
2020-04-24 17:02:03 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,16 @@
 require 'pry' 
 
+=begin
 def reverse_each_word(string)
   sentence_array = string.split() 
-#  reverse_sentence = []
-  reverse_sentence = sentence_array.each do |word|
-    word.reverse
+  reverse_sentence = []
+  sentence_array.each do |word|
+    reverse_sentence << word.reverse
   end
   reverse_sentence.join(" ")
 end
+=end
 
-=begin
 def reverse_each_word(string)
   sentence_array = string.split() 
   reverse_sentence = sentence_array.collect do |word|
@@ -17,4 +18,3 @@ def reverse_each_word(string)
   end
   reverse_sentence.join(" ")
 end
-=end
