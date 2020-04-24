391a979514ce8309e652866a0955ab8c8b2c1da5
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2020-04-24 15:20:06 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -13,9 +13,5 @@
 #using collect
 def reverse_each_word(string)
   array = string.split
-  reversed_array = []
-  array.collect do |word|
-    reversed_array << word.reverse
-  end
-  reversed_array.join(" ")
+  array.collect {|word| word.reverse.join(" ")
 end
